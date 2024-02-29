31557a11f692169901e4588431d8d0eb4d526c9e
Mahdinkh7 <mahdinkh7@gmail.com>
2024-02-29 23:46:50 +0330

    done

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Speaker',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: OnboardPage());
+        home: const OnboardPage());
   }
 }
--- lib/pages/onboard.dart
+++ lib/pages/onboard.dart
@@ -5,6 +5,73 @@ class OnboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(top: 50),
+                      child: Image.asset(
+                        'assets/logo.png',
+                        width: 150,
+                        height: 60,
+                      )),
+                  Image.asset('assets/1.png'),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Text(
+                      "Learn a language\nin 3 minute a day",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 28),
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF007AFF),
+                        borderRadius: BorderRadius.circular(15)),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: const Center(
+                      child: Text(
+                        "Start learning",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already, have an account?',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16),
+                      ),
+                      SizedBox(width: 6),
+                      Text(
+                        "Log in",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.blue,
+                            fontSize: 16),
+                      )
+                    ],
+                  )
+                ]),
+          ),
+        ));
   }
 }
